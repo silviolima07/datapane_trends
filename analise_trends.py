f82327bcf12c841702fdcf63c97b965ec4028dd2
silviolima07 <silvio.clima@unitau.com.br>
2021-04-14 16:34:32 -0300

    Ajustes finais

--- analise_trends.py
+++ analise_trends.py
@@ -61,8 +61,6 @@ print(df_region.head(20))
 
 # Fig1 é gráfico gerado do interesse dos termos escolhidos ao longo do tempo.
 
-fig1_bol = px.line(df,df.date, 'jair bolsonaro', title='Bolsonaro x Date')
-
 fig1_covid = px.line(df,df.date, 'covid', title='Covid x Date')
 
 fig1_lock = px.line(df,df.date, 'lockdown', title='Lockdown x Date')
@@ -72,8 +70,6 @@ fig1_lock = px.line(df,df.date, 'lockdown', title='Lockdown x Date')
 
 # Fig2 é gráfico gerado do interesse dos termos escolhidos por Estado.
 
-fig2_bol = px.bar(df_region, x=df_region.estado, y="jair bolsonaro", title = "Bolsonaro x Estado")
-
 fig2_covid = px.bar(df_region, x=df_region.estado, y="covid", title = "Covid x Estado")
 
 fig2_lock = px.bar(df_region, x=df_region.estado, y="lockdown", title = "Lockdown x Estado")
@@ -82,9 +78,6 @@ fig2_lock = px.bar(df_region, x=df_region.estado, y="lockdown", title = "Lockdow
 ##########################
 
 # Fig3 é gráfico gerado do interesse dos termos escolhidos por Estado.
-
-fig3_bol = px.treemap(df_region, path=[px.Constant('Termo Jair Bolsonaro pelo BRASIL'), df_region.index], values='jair bolsonaro',
-                  color='jair bolsonaro')
                   
 fig3_covid = px.treemap(df_region, path=[px.Constant('Termo Covid no BRASIL'), df_region.index], values='covid',
                   color='covid')
@@ -118,6 +111,25 @@ fig_clouds.show()
 
 #######################
 
+search_list = ["LINGUAGEM R", "LINGUAGEM PYTHON"]
+pytrends.build_payload(search_list, timeframe='today 5-y', geo='BR')
+
+df_linguagens_ot = pd.DataFrame(pytrends.interest_over_time()).drop(columns='isPartial')
+# Gerar a coluna year com o data completa vinda do index
+df_linguagens_ot['date'] = df_linguagens_ot.index
+
+fig_R = px.line(df_linguagens_ot,df_linguagens_ot.index,'LINGUAGEM R', title='Linguagem R x Date', labels= {'index':'date'})
+fig_R
+
+fig_python = px.line(df_linguagens_ot,df_linguagens_ot.index,'LINGUAGEM PYTHON', title='Linguagem PYTHON x Date', labels= {'index':'date'})
+fig_python
+
+
+
+
+#######################
+
+
 # Create report
 
 r = dp.Report(
@@ -127,11 +139,17 @@ r = dp.Report(
                "#### AWS, AZURE e GCP ",
                dp.Plot(fig_clouds)]
      ),
+     dp.Page(
+       label='Trend search Linguagens',
+       blocks=[
+               "#### Scatter Plot -> interest_over_time of Python",
+               dp.Plot(fig_python),
+               "### Scatter Plot -> interest_over_time of R",
+               dp.Plot(fig_R)]
+     ),
     dp.Page(
        label='Covid',
        blocks=[
-               #"#### Heatmap do termo Covid no Brasil", 
-               #dp.Plot(mapa_covid),
                "#### Scatter Plot -> interest_over_time", 
                dp.Plot(fig1_covid),
                "#### Bar Plot -> interest_by_region", 
@@ -141,23 +159,8 @@ r = dp.Report(
                ]
      ),
     dp.Page(
-       label='Bolsonaro',
-       blocks=[
-               #"#### Heatmap do termo Jair Bolsonaro no Brasil", 
-               #dp.Plot(mapa_jair_bolsonaro),
-               "#### Scatter Plot -> interest_over_time", 
-               dp.Plot(fig1_bol),
-               "#### Bar Plot -> interest_by_region", 
-               dp.Plot(fig2_bol),
-               "#### Treemap - > interest_by_region", 
-               dp.Plot(fig3_bol)
-               ]
-     ),
-    dp.Page(
        label='Lockdown',
        blocks=[
-               #"#### Heatmap do termo Lockdown no Brasil", 
-               #dp.Plot(mapa_lockdown),
                "#### Scatter Plot -> interest_over_time", 
                dp.Plot(fig1_lock),
                "#### Bar Plot - > interest_by_region", 
@@ -169,6 +172,6 @@ r = dp.Report(
     )
 r
 # Publish
-r.publish(name=f'Google Trends com Python Pytrends', open = True, description='Analisando de jan/2018 até hoje, os termos: Covid, Jair Bolsonaro e Lockdown')
+r.publish(name=f'Google Trends com Python Pytrends', open = True, description='Analisando: interest_over_time e interest_by_region')
 
      
